2ff1017dc57301789c8842c4afa313eb8f219f15
Pedro Ribeiro Filho <97203584+Pyyyter@users.noreply.github.com>
2024-04-16 04:17:45 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://pyyyter.github.io',
-    base: 'portfolio',
+    site: 'https://pyyyter.github.io'
   })
